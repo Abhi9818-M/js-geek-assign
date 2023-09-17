cd930b47a3fe028c51dfd7ce4de8002b08257046
Abhi9818-M <abhishekmishra64thps@gmail.com>
2023-09-17 13:56:50 +0530

    updated

--- githubfinder/script.js
+++ githubfinder/script.js
@@ -53,54 +53,84 @@ function getUser(searchValue) {
             }
             else{
 
-                cardContainer.innerHTML = "";
+                cardContainer.innerHTML = `<div class="firstrow">
+                                            <div class="firstRow_image">
+                                                <img src="${result.avatar_url}" alt="">
+                                                <div class="userId">
+                                                    <h2>${result.name}</h2>
+                                                    <a href="${result.html_url}">${result.login}</a>
+                                                    <p>${result.bio}</p>
+                                                </div>
+                                            </div>
+                                            <div class="date">
+                                                Created At :-
+                                                ${new Date(data.created_at).toLocaleDateString('en-US')}
+                                            </div>
+                                        </div>
+                                        <div class="secondRow">
+                                            <div class="follower">
+                                                Followers
+                                                <p>${result.followers}</p></div>
+                                            <div class="following">
+                                                Following 
+                                                <p>${result.following}</p></div>
+                                            <div class="repo">
+                                                Repo 
+                                                <p>${result.public_repos}</p></div>
+                                        </div>
+                                        <div class="thirdRow">
+                                            <div class="thirdRow_data">
+                                                <div class="location"><i class="fa-solid fa-location-dot"></i>${result.location}</div>
+                                                <div class="company"><i class="fa-solid fa-building"></i>${result.company}</div>
+                                            </div>
+                                        </div>`;
+                                       
+                // const card1 = document.createElement("div")
+                // const card2 = document.createElement("div")
+                // const imagediv = document.createElement("div")
+                // const userDiv = document.createElement("div")
+                // const dateDiv = document.createElement("div")
+                // const heading = document.createElement("h2")
+                // const bio = document.createElement("p")
+                // const date = document.createElement("p")
+                // const image = document.createElement("img")
+                // const link = document.createElement("a")
+                // const repo = document.createElement("div")
+                // const follower = document.createElement("div")
+                // const following = document.createElement("div")
 
-                const card1 = document.createElement("div")
-                const card2 = document.createElement("div")
-                const imagediv = document.createElement("div")
-                const userDiv = document.createElement("div")
-                const dateDiv = document.createElement("div")
-                const heading = document.createElement("h2")
-                const bio = document.createElement("p")
-                const date = document.createElement("p")
-                const image = document.createElement("img")
-                const link = document.createElement("a")
-                const repo = document.createElement("div")
-                const follower = document.createElement("div")
-                const following = document.createElement("div")
+                // card1.classList.add("firstRow")
+                // userDiv.classList.add("userId")
+                // card2.classList.add("secondRow")
+                // imagediv.classList.add("firstRow_image")
+                // dateDiv.classList.add("date")
 
-                card1.classList.add("firstRow")
-                userDiv.classList.add("userId")
-                card2.classList.add("secondRow")
-                imagediv.classList.add("firstRow_image")
-                dateDiv.classList.add("date")
-
-                image.src = result.avatar_url
-                link.href = result.html_url 
-                link.innerHTML = result.login
-                date.innerText = result.created_at
-                bio.innerText = result.bio
-                heading.innerText = result.name
-                repo.innerText = result.public_repos
+                // image.src = result.avatar_url
+                // link.href = result.html_url 
+                // link.innerHTML = result.login
+                // date.innerText = result.created_at
+                // bio.innerText = result.bio
+                // heading.innerText = result.name
+                // repo.innerText = result.public_repos
                 
-                follower.innerText = result.followers
+                // follower.innerText = result.followers
                 
-                following.innerText = result.following
+                // following.innerText = result.following
                 
-                imagediv.appendChild(image)
-                userDiv.appendChild(heading)  
-                userDiv.appendChild(link)
-                dateDiv.appendChild(date)
-                userDiv.appendChild(bio)
-                card2.appendChild(repo)
-                card2.appendChild(follower)
-                card2.appendChild(following)
-                card1.appendChild(imagediv) 
-                cardContainer.appendChild(card1) 
-                card1.appendChild(imagediv)
-                imagediv.appendChild(userDiv)
-                card1.appendChild(dateDiv)
-                cardContainer.appendChild(card2)       
+                // imagediv.appendChild(image)
+                // userDiv.appendChild(heading)  
+                // userDiv.appendChild(link)
+                // dateDiv.appendChild(date)
+                // userDiv.appendChild(bio)
+                // card2.appendChild(repo)
+                // card2.appendChild(follower)
+                // card2.appendChild(following)
+                // card1.appendChild(imagediv) 
+                // cardContainer.appendChild(card1) 
+                // card1.appendChild(imagediv)
+                // imagediv.appendChild(userDiv)
+                // card1.appendChild(dateDiv)
+                // cardContainer.appendChild(card2)       
 
             }
         }
