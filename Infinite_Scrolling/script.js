6edfa6d3060d0793c5df2e3d94184a52e2eea6e9
Abhi9818-M <abhishekmishra64thps@gmail.com>
2023-09-20 12:11:59 +0530

    updat

--- Infinite_Scrolling/script.js
+++ Infinite_Scrolling/script.js
@@ -1,27 +1,36 @@
-const url = "https://api.unsplash.com/photos/random/?client_id=_DDIVJSgdK-GI1wA3aHOtxC9YTt8tCY6-4jMk7guznY&count=100";
+const container = document.getElementById("container");
 
-const container = document.getElementById("container")
-const fetchImage = () =>{
-    const image = fetch(url)
-    image.then((data)=>{
-        console.log(data);
-        return data.json()
-    }).then((data)=>{
-        console.log(data);
-        data.map((ele)=>{
-            console.log(ele.urls.small);
-            const img = document.createElement("img")
-            img.classList.add("img")
-            img.src = ele.urls.small
-            container.appendChild(img)
-        })
+const url =
+  "https://api.unsplash.com/photos/random/?client_id=_DDIVJSgdK-GI1wA3aHOtxC9YTt8tCY6-4jMk7guznY&count=10";
+
+const fetchImage = () => {
+  const image = fetch(url);
+  image
+    .then((data) => {
+      console.log(data);
+      return data.json();
     })
-    console.log(image);
-}
-window.addEventListener("scroll",()=>{
-    console.log(window.innerHeight);
-    if(window.scrollY+window+innerHeight >= document.documentElement.scrollHeight){
-        fetchImage();
-    }
-})
-fetchImage()
+    .then((data) => {
+      console.log(data);
+      data.map((ele) => {
+        console.log(ele.urls.small);
+        const img = document.createElement("img");
+        img.src = ele.urls.small;
+        img.classList.add("img")
+        container.appendChild(img);
+      });
+    });
+  console.log(image);
+};
+
+window.addEventListener("scroll", () => {
+  console.log(window.innerHeight);
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    fetchImage();
+  }
+});
+
+fetchImage();
